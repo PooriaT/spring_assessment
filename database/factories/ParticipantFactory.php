e841bc6674325f2f800d44210704a7beaf788023
PooriaT <pooria_taghdiri@hotmail.com>
2023-12-22 17:40:00 -0800

    Adding Participant Model, Factory, and Migration

---
+++ database/factories/ParticipantFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Participant>
+ */
+class ParticipantFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+            'age' => $this->faker->numberBetween(18, 60),
+            'points' => $this->faker->numberBetween(0, 100),
+            'address' => $this->faker->address(),
+        ];
+    }
+}
